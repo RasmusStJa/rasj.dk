ab648e46d57fcb7e2e59e7c217391f7da42d9be0
RasmusStJa <180268023+RasmusStJa@users.noreply.github.com>
2025-09-29 15:29:19 +0200

    Update main.go
    appendBtn now gets the address to the button, which means it now appends it correctly
    Rewrote the HTML() functions to return the HTML as a string instead of printing it
    Renamed the dir string slice to dirs in the link struct

--- main.go
+++ main.go
@@ -13,18 +13,25 @@ var legalTypes = []string{"button", "collapse", "end"}
 
 type link struct {
 	baseURL string
-	dir     []string
+	dirs    []string
 }
 
-func (l *link) resetDir()            { l.dir = nil }
-func (l *link) addDir(newDir string) { l.dir = append(l.dir, newDir) }
+func (l *link) resetDir()            { l.dirs = nil }
+func (l *link) addDir(newDir string) { l.dirs = append(l.dirs, newDir) }
 func (l *link) rmDirs(count int) {
-	length := len(l.dir)
+	length := len(l.dirs)
 	if length == count {
 		l.resetDir()
 	} else if count > 0 && length > count {
-		l.dir = l.dir[:length-count]
+		l.dirs = l.dirs[:length-count]
 	}
+}
+func (l link) get() string {
+	result := l.baseURL
+	for _, dir := range l.dirs {
+		result += dir
+	}
+	return result
 }
 
 type navBtn struct {
@@ -34,62 +41,74 @@ type navBtn struct {
 func (btn *navBtn) setName(name string)    { btn.name = name }
 func (btn *navBtn) setDir(dir string)      { btn.dir = dir }
 func (btn *navBtn) setType(btnType string) { btn.btnType = btnType }
-func (btn navBtn) HTML(url *link) {
+func (btn navBtn) HTML(url *link) string {
+	var result string
 	switch btn.btnType {
 	case legalTypes[0]: //button
-		fmt.Println("<button>", btn.name, "</button>")
+		result = "<button href='" + url.get() + btn.dir + "'>" + btn.name + "</button>"
 	case legalTypes[1]: //collapse
-		fmt.Println("<button id='toggleButton'>", btn.name, ":", "</button>")
-		fmt.Println("<div id='collapsibleDiv' style='display: none;'>")
 		url.addDir(btn.dir)
+		result = "<button id='toggleButton'>" + btn.name + ":" + "</button>" + "<div id='collapsibleDiv' style='display: none;'>"
 	case legalTypes[2]: //end
 		//TODO: come up with better name than "name" (should be smth like amount of ends)
 		switch strings.ToLower(btn.name) {
 		case "all": //remove everything
-			for range len(url.dir) {
-				fmt.Println("</div>")
+			for range len(url.dirs) {
+				result += "</div>"
 			}
+
 			url.resetDir()
 		case "": //remove 1 time
-			fmt.Println("</div>")
-			url.rmDirs(1)
+			if len(url.dirs) >= 1 {
+				url.rmDirs(1)
+				result = "</div>"
+			}
 		default: //remove n times
-			count, err := strconv.Atoi(btn.name)
+			count, err := strconv.Atoi(btn.name) //string (anything) to int
 			if err != nil {
 				panic(err)
 			}
-			for range count {
-				fmt.Println("</div>")
+			if len(url.dirs) >= count {
+				for range count {
+					result += "</div>"
+				}
+				url.resetDir()
 			}
-			url.resetDir()
 		}
 	}
-
+	return result
 }
 
 type navBar struct{ btnContainer []navBtn }
 
-func (nb navBar) appendBtn(btn navBtn) {
+func (nb *navBar) appendBtn(btn navBtn) {
 	nb.btnContainer = append(nb.btnContainer, btn)
 }
-func (nb navBar) HTML() {
+func (nb navBar) HTML() string {
+	var result string
 	url := link{baseURL: "https://rasj.dk"}
 	for _, btn2 := range nb.btnContainer {
 		if slices.Contains(legalTypes, btn2.btnType) {
-			btn2.HTML(&url)
+			result += btn2.HTML(&url)
 			continue
 		}
 		fmt.Println(btn2.name, "has illegal type:", btn2.btnType)
 		break
 	}
+	return result
 }
 
+/*
 func main() {
+	url := link{baseURL: "https://rasj.dk"}
+	navBtn{name: "Forside", dir: "/", btnType: "button"}.HTML(&url)
 	var mainNavBar navBar
 	mainNavBar.appendBtn(navBtn{name: "Forside", dir: "/", btnType: "button"})
+	mainNavBar.appendBtn(navBtn{name: "Help", dir: "/help", btnType: "button"})
 	mainNavBar.appendBtn(navBtn{name: "Spil", dir: "/spil", btnType: "collapse"})
 	mainNavBar.appendBtn(navBtn{name: "Wordle", dir: "/wordle", btnType: "button"})
 	mainNavBar.appendBtn(navBtn{btnType: "end"})
 
-	mainNavBar.HTML()
+	fmt.Println(mainNavBar.HTML())
 }
+*/
