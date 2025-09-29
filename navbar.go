3d4519bf45235215f1a3f4d77d7320cd248ccb64
RasmusStJa <180268023+RasmusStJa@users.noreply.github.com>
2025-09-29 15:30:54 +0200

    Update and rename main.go to navbar.go
    Removed the main function

--- navbar.go
+++ navbar.go
@@ -97,18 +97,3 @@ func (nb navBar) HTML() string {
 	}
 	return result
 }
-
-/*
-func main() {
-	url := link{baseURL: "https://rasj.dk"}
-	navBtn{name: "Forside", dir: "/", btnType: "button"}.HTML(&url)
-	var mainNavBar navBar
-	mainNavBar.appendBtn(navBtn{name: "Forside", dir: "/", btnType: "button"})
-	mainNavBar.appendBtn(navBtn{name: "Help", dir: "/help", btnType: "button"})
-	mainNavBar.appendBtn(navBtn{name: "Spil", dir: "/spil", btnType: "collapse"})
-	mainNavBar.appendBtn(navBtn{name: "Wordle", dir: "/wordle", btnType: "button"})
-	mainNavBar.appendBtn(navBtn{btnType: "end"})
-
-	fmt.Println(mainNavBar.HTML())
-}
-*/
