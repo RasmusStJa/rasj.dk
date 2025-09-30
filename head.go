5eb38f5f4a2e7adc08d7bbd95d3d484cc61b58e1
RasmusStJa <180268023+RasmusStJa@users.noreply.github.com>
2025-09-30 03:24:08 +0200

    Create head.go
    Init commit for head.go
    head.go generates the header for each file.
    It does so by using the element struct from html.go, and its subsequent methods.

---
+++ head.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"runtime"
+	"strings"
+)
+
+type favicon struct {
+	ftype string
+	fpath string
+}
+type header struct {
+	fv        favicon
+	stylepath string
+}
+
+func (h header) HTML() string {
+	var result string
+
+	stylesheet := element{tag: "link"}
+	stylesheet.attributes = []attribute{{name: "href", value: "https://rasj.dk" + h.stylepath}, {name: "rel", value: "stylesheet"}}
+
+	charset := element{tag: "meta"}
+	charset.attributes = []attribute{{name: "charset", value: "utf-8"}}
+
+	viewport := element{tag: "meta"}
+	viewport.attributes = []attribute{{name: "name", value: "viewport"}, {name: "content", value: "width=device-width, initial-scale=1"}}
+
+	icon := element{tag: "link"}
+	icon.attributes = []attribute{{name: "rel", value: "icon"}, {name: "type", value: "image/svg+xml"}, {name: "href", value: "https://rasj.dk" + h.fv.fpath}}
+
+	mathjax := element{tag: "script"}
+	mathjax.attributes = []attribute{{name: "id", value: "MathJax-script"}, {name: "async"}, {name: "src", value: "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"}}
+
+	_, filename, _, _ := runtime.Caller(1)
+	title := element{tag: "title"}
+	title.innerText = "rasj.dk - " + strings.Split(filename, ".")[0]
+
+	result += stylesheet.HTML()
+	result += charset.HTML()
+	result += viewport.HTML()
+	result += icon.HTML()
+	result += mathjax.HTML()
+	result += title.HTML()
+
+	return result
+}
