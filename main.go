70cf13b28a4dff2c22023cc67c0faa3a77fe51d1
RasmusStJa <180268023+RasmusStJa@users.noreply.github.com>
2025-09-30 11:24:38 +0200

    Update main.go
    main index has some example code now, and can be ran as a server

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	//"log"
-	//"net/http"
+	"io"
+	"net/http"
+	"os"
 )
 
-func main() {
-	url := link{baseURL: "https://rasj.dk"}
-	navBtn{name: "Forside", dir: "/", btnType: "button"}.HTML(&url)
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Got / request")
 	var mainNavBar navBar
 	mainNavBar.appendBtn(navBtn{name: "Forside", dir: "/", btnType: "button"})
 	mainNavBar.appendBtn(navBtn{name: "Help", dir: "/help", btnType: "button"})
@@ -16,5 +17,18 @@ func main() {
 	mainNavBar.appendBtn(navBtn{name: "Wordle", dir: "/wordle", btnType: "button"})
 	mainNavBar.appendBtn(navBtn{btnType: "end"})
 
-	fmt.Println(mainNavBar.HTML())
+	w.Header().Add("Content-Type", "text/html")
+	io.WriteString(w, "<h1>Example page</h1>"+mainNavBar.HTML())
+}
+
+func main() {
+	http.HandleFunc("/", getRoot)
+
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Println("Server is closed")
+	} else if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
