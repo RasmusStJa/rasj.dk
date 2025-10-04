1f574b1a5427e1cc403e1bdff04b5ea5ea23be5f
RasmusStJa <180268023+RasmusStJa@users.noreply.github.com>
2025-10-04 12:10:07 +0200

    Update main.go
    Mainly added an about page
    Also minorly improved the isnowaprime page

--- main.go
+++ main.go
@@ -11,41 +11,35 @@ import (
 )
 
 const stylepth string = "style.css"
-/*
-func getNavbar() navBar {
-	var mainNavBar navBar
-	mainNavBar.AppendBtn(navBtn{name: "Forside", dir: "/", btnType: "button"})
-	mainNavBar.AppendBtn(navBtn{name: "Help", dir: "/help", btnType: "button"})
-	mainNavBar.AppendBtn(navBtn{name: "Spil", dir: "/spil", btnType: "collapse"})
-	mainNavBar.AppendBtn(navBtn{name: "Wordle", dir: "/wordle", btnType: "button"})
-	mainNavBar.AppendBtn(navBtn{btnType: "end"})
-	return mainNavBar
-}
-*/
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Got / request")
-	head := header{stylepath: stylepth}
 
+/*
+	func getNavbar() navBar {
+		var mainNavBar navBar
+		mainNavBar.AppendBtn(navBtn{name: "Forside", dir: "/", btnType: "button"})
+		mainNavBar.AppendBtn(navBtn{name: "Help", dir: "/help", btnType: "button"})
+		mainNavBar.AppendBtn(navBtn{name: "Spil", dir: "/spil", btnType: "collapse"})
+		mainNavBar.AppendBtn(navBtn{name: "Wordle", dir: "/wordle", btnType: "button"})
+		mainNavBar.AppendBtn(navBtn{btnType: "end"})
+		return mainNavBar
+	}
+*/
+func getAbout(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Got /about request")
+	w.Header().Add("Content-Type", "text/html")
+
+	head := header{stylepath: stylepth}
 	pageBody := element{tag: "body"}
 
-	pageBody.AppendChild(element{tag: "h1", innerText: "Example page"})
-	pageBody.AppendChild(element{tag: "p", innerText: "The time is currently " + time.Now().Format("15:04")})
-	pageBody.AppendChild(element{tag: "button", innerText: "Is now a prime?", attributes: []attribute{{name: "onclick", value: "location.href=\"https://pi.rasj.dk/isnowaprime\""}}})
-	
-	w.Header().Add("Content-Type", "text/html")
+	pageBody.AppendChild(element{tag: "h1", innerText: "About"})
+	pageBody.AppendChild(element{tag: "p", innerText: "This server is running on a Raspberry Pi 5 I have at home.<br>It's running Go for the backend, where I've written some code to generate the HTML. You can find that ",
+		children: []element{{tag: "a", innerText: "here.", attributes: []attribute{{name: "href", value: "https://github.com/RasmusStJa/rasj.dk"}}}}})
+
 	io.WriteString(w, head.HTML())
 	io.WriteString(w, pageBody.HTML())
-	//io.WriteString(w, ().HTML())
 }
 
 func isprime(n int) bool {
-	if n < 2 {
-		return false
-	}
-	if n == 2 {
-		return true
-	}
-	if n%2 == 0 {
+	if n < 2 || n%2 == 0 {
 		return false
 	}
 
@@ -82,9 +76,8 @@ func getPrime(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
 
 	pageBody := element{tag: "body"}
-	head := header{stylepath: stylepth}
 
-	now, _ := strconv.Atoi(time.Now().Format("0601021504"))
+	now, _ := strconv.Atoi("20" + time.Now().Format("0601021504"))
 	pageBody.AppendChild(element{tag: "h1", innerText: "Is now a prime?"})
 	pageBody.AppendChild(element{tag: "p", innerText: "Is " + strconv.Itoa(now) + " a prime?"})
 	if isprime(now) {
@@ -103,24 +96,43 @@ func getPrime(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	/*
-		var primes []element
-		offset := 1
-		for len(primes) < 3 {
+		var prime int
+		var offset uint16 = 1
+		for {
 			d1 := time.Now().Add(time.Minute * time.Duration(offset))
 			d2, _ := strconv.Atoi(d1.Format("0601021504"))
 			if isprime(d2) {
-				primes = append(primes, element{tag: "p", innerText: d1.Format("2006-01-02 15:04")})
+				prime = d2
+				break
 			}
 			offset++
 		}
-		pageBody.AppendChild(element{tag: "p", innerText: "Here are the next 3 prime times:"})
-		pageBody.AppendChildren(primes)*/
- io.WriteString(w, head.HTML())
+		pageBody.AppendChild(element{tag: "p", innerText: "Here is the next prime:"})
+		pageBody.AppendChild(element{tag: "p", innerText: strconv.Itoa(prime)})*/
+
 	io.WriteString(w, pageBody.HTML())
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Got / request")
+	head := header{stylepath: stylepth}
+
+	pageBody := element{tag: "body"}
+
+	pageBody.AppendChild(element{tag: "h1", innerText: "Example page"})
+	pageBody.AppendChild(element{tag: "p", innerText: "The time is currently " + time.Now().Format("15:04")})
+	pageBody.AppendChild(element{tag: "button", innerText: "About", attributes: []attribute{{name: "onclick", value: "location.href=\"https://pi.rasj.dk/about\""}}})
+	pageBody.AppendChild(element{tag: "button", innerText: "Is now a prime?", attributes: []attribute{{name: "onclick", value: "location.href=\"https://pi.rasj.dk/isnowaprime\""}}})
+
+	w.Header().Add("Content-Type", "text/html")
+	io.WriteString(w, head.HTML())
+	io.WriteString(w, pageBody.HTML())
+	//io.WriteString(w, ().HTML())
 }
 
 func main() {
 	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/about", getAbout)
 	http.HandleFunc("/isnowaprime", getPrime)
 
 	err := http.ListenAndServe(":3000", nil)
